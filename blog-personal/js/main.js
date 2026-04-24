3e6282642785014667a7c9779c292435e7284801
Anahi <leslianahi102@gmail.com>
2026-04-24 15:21:20 -0600

    first commit

---
+++ blog-personal/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Inicializar animaciones AOS
+    AOS.init({
+        duration: 1000,
+        once: false,
+        mirror: true,
+        offset: 80
+    });
+
+    console.log("Blog Personal de Ramon R. cargado correctamente.");
+});
